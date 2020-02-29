57da2f7c5c511d550a0502b21f25be087adbc76b
Joey Register <josephregister1258@gmail.com>
2020-02-29 13:02:45 -0500

    sample schema created

---
+++ seeders/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS adventure_db;
+
+CREATE DATABASE adventure_db;
+
+CREATE TABLE scenarios(
+    id integer not null auto_increment,
+    scenario varchar(255),
+    choiceA integer not null,
+    choiceB integer not null,
+    primary key(id)
+);

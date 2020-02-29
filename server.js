e7585eb0184f1266c13e4067b6724de416aa8918
Joey Register <josephregister1258@gmail.com>
2020-02-29 16:28:14 -0500

    configured hbs helper fn's and updated {{index}}

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ app.set('view engine', 'handlebars')
 
 //custom handlebars helper to split scenario column
 Handlebars.registerHelper('splitScene', (scene)=>{
-    return scene.split(",").slice(-1,str.length-2)
+    const str = scene.split(",")
+    return scene.split(",").slice(0,str.length-2)
+});
+Handlebars.registerHelper('splitA', (optionA)=>{
+    const str = optionA.split(",")
+    return optionA.split(",").slice(1,str.length-1)
+});
+Handlebars.registerHelper('splitB', (optionB)=>{
+    const str = optionB.split(",")
+    return optionB.split(",").slice(2,str.length)
 });
 // app.use(routes)
 const connection = mysql.createConnection({

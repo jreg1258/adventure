cdaec20e08206fa765afafb21f1e5e02880576ee
Joey Register <josephregister1258@gmail.com>
2020-03-01 22:07:18 -0500

    added sidebar and animation space

--- public/js/choice.js
+++ public/js/choice.js
@@ -3,8 +3,22 @@ const optionB = $("#b")
 const gameSpace = $("#gameSpace")
 const a = optionA.val().trim()
 const b = optionB.val().trim()
+let sprite; //we will use this variable to clear the setInterval()
 
+const stopAnimate = ()=>{
+    clearInterval(sprite);
+};
 
+const animateScript = ()=>{
+    let position = 256; //start position for the image slicer
+    
+    sprite = setInterval(()=>{
+        document.getElementById("image").style.backgroundPosition = `-${position}px 0px`; 
+            if (position < 1536)
+                { position = position + 256;}//we increment the position by 256 each time
+            else
+                { position = 256; }//reset the position to 256px, once position exceeds 1536px
+},100)};
 
 $(gameSpace).on("submit", (event)=>{
     event.preventDefault()

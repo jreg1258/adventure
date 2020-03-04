72fc3458c59386cd6fd4d131ecf91a25555b5a27
andrewreedy <reedyandrew11@gmail.com>
2020-03-03 19:36:14 -0500

    password delete

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'Kittylitter11',
+  password: 'Password',
   database: 'adventure_db'
 })
 

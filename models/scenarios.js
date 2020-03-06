2f16adf0554e2c20db6b41f8f3ca92e372bc1e81
Joey Register <josephregister1258@gmail.com>
2020-03-05 20:47:01 -0500

    changes to js

--- models/scenarios.js
+++ models/scenarios.js
@@ -1,10 +1,10 @@
-module.exports = function(sequelize, DataTypes) {
-    var Scenarios = sequelize.define("Scenarios", {
-      // Giving the Author model a name of type STRING
-      scenario: DataTypes.STRING,
-      choiceA: DataTypes.INTEGER,
-      choiceB: DataTypes.INTEGER
-    });
+module.exports = function (sequelize, DataTypes) {
+  var Scenarios = sequelize.define('Scenarios', {
+    // Giving the Author model a name of type STRING
+    scenario: DataTypes.STRING,
+    choiceA: DataTypes.INTEGER,
+    choiceB: DataTypes.INTEGER
+  })
 
-    return Author;
-  };
+  return Author
+}
